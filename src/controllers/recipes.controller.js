9ecf9b65c21fd1a23a10e19bdd3e42a54b79faa0
AntoineBonamy <bonamy.antoine@hotmail.fr>
2024-01-15 18:20:59 +0100

    :sparkles: En tant qu’admin, je souhaite pouvoir créer une recette. Ajout de la création de recette sur la route /recipes

--- src/controllers/recipes.controller.js
+++ src/controllers/recipes.controller.js
@@ -1,6 +1,24 @@
 const supabase = require("../../supabase");
 
-const getRecipes = async (req, res) => {
+// Create
+const createRecipe = async (req, res) => {
+    const { name, type, nbIngredient, cookingTime, description, picture } = req.body;
+    await supabase
+        .from("recipes")
+        .insert([
+            {name: name, type: type, nb_ingredient: nbIngredient, cooking_time: cookingTime, description: description, picture: picture}
+        ])
+        .select()
+        .then(() => {
+            res.status(201).send("Created");
+        })
+        .catch((err) => {
+            res.status(500).send("Error creating recipe : ", err);
+        })
+}
+
+// Read
+const getAllRecipes = async (req, res) => {
     await supabase
         .from("recipes")
         .select("*")
@@ -12,6 +30,21 @@ const getRecipes = async (req, res) => {
         })
 };
 
+const getRecipeById = async (req, res) => {
+    const { id } = req.params;
+    await supabase
+        .from("recipes")
+        .select("*")
+        .eq("id", id)
+        .then((recipe) => {
+            res.status(200).json(recipe)
+        })
+        .catch((err) => {
+            res.status(500).send("Error getting recipe by id : ", err);
+        })
+}
 module.exports = {
-    getRecipes
+    createRecipe,
+    getAllRecipes,
+    getRecipeById
 }
