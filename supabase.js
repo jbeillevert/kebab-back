caac042349365b3f9fa624f75f925d1016323b43
Med Ars <mehdi.arroussi@ynov.com>
2024-01-16 18:19:46 +0100

    Fix tests to with env

--- supabase.js
+++ supabase.js
@@ -1,6 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 require("dotenv").config();
 
+console.log(process.env.API_URL);
+
 const supabase = createClient(
   process.env.API_URL,
   process.env.API_KEY
